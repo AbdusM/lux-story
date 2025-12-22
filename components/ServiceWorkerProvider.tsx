daa97481bb92aa011bb6a0835e364e7af097bd94
Abdus-Salaam Muwwakkil <amuwwakkil91@gmail.com>
2025-12-21 19:09:07 -0500

    chore: force service worker unregistration and bump version to 2.0.1 to clear cache

--- components/ServiceWorkerProvider.tsx
+++ components/ServiceWorkerProvider.tsx
@@ -5,12 +5,18 @@ import { registerServiceWorker } from '@/lib/service-worker'
 
 export function ServiceWorkerProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    // FORCE UNREGISTER for development debugging
+    // FORCE UNREGISTER to clear stale caches - The user is seeing old interface elements
+    // We are currently disabling PWA features to ensure the latest deployment is always served
+    import('@/lib/service-worker').then(mod => mod.unregisterServiceWorker())
+
+    // Legacy logic preserved but disabled for now:
+    /*
     if (process.env.NODE_ENV === 'development') {
       import('@/lib/service-worker').then(mod => mod.unregisterServiceWorker())
     } else {
       registerServiceWorker()
     }
+    */
   }, [])
 
   return <>{children}</>
